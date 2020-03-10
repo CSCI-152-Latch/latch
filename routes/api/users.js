3fd349f5e8832d53a6c18c3baab38d0a2ddacf81
James Hernandez <hernandezjn97@gmail.com>
2020-03-10 16:50:46 -0700

    updated something

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,8 +6,8 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator"); // better understanding on npm documentation
 const User = require("../../models/User"); //importing teh user model
-const Friend = require('../../models/Friend');
-const auth = require('../../middleware/auth');
+const Friend = require("../../models/Friend");
+const auth = require("../../middleware/auth");
 //express-validator helps making sure that the fileds are filled correctly
 
 // Type:        Post
@@ -85,7 +85,23 @@ router.post(
         user: {
           id: user.id
         }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      //res.send("User registered");
+    } catch (err) {
+      //if something goes wrong here then its a server error
+      console.error(err.message);
+      res.status(500).send("server error status 500");
     }
+  }
 );
 
 // Type:        Post
@@ -93,88 +109,91 @@ router.post(
 // Purpose:     Creating a new friends list for a new user
 // Acess:       Private
 router.post(
-    '/friend', 
-    // auth,
-    [
-        check('user').notEmpty()
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ error: errors.array()});
-        }
-
-        try {
-            const { user, friends, requesting, spending } = req.body;
-            const friend = new Friend({
-                user,
-                friends,
-                requesting,
-                spending
-
-            });
-
-            await friend.save();
-            return res.send(friend);
-        }
-        catch (err) {
-            res.status(500).send(err);
-        }
+  "/friend",
+  // auth,
+  [check("user").notEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
     }
+
+    try {
+      const { user, friends, requesting, spending } = req.body;
+      const friend = new Friend({
+        user,
+        friends,
+        requesting,
+        spending
+      });
+
+      await friend.save();
+      return res.send(friend);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
 );
 
 // Type:        GET
 // Where:       api/user
 // Purpose:     Adding new friends
 // Acess:       Private
-router.post('/add', async (req, res) => {
-    try {
-        const { requesterUser, spenderUser } = req.body;
+router.post("/add", async (req, res) => {
+  try {
+    const { requesterUser, spenderUser } = req.body;
 
-        const exist = await Friend.findOne({ user: spenderUser, spending: requesterUser });
-        if (exist) {
-           return res.send('Already requested!');
-        }
-
-        const requester = await Friend.findOne({ user: requesterUser });
-        const spender = await Friend.findOne({ user: spenderUser });
-
-        requester.requesting.push(spenderUser);
-        requester.save();
-        spender.spending.push(requesterUser);
-        spender.save();
-
-        res.send({requester, spender});
+    const exist = await Friend.findOne({
+      user: spenderUser,
+      spending: requesterUser
+    });
+    if (exist) {
+      return res.send("Already requested!");
     }
-    catch (err) {
-        res.status(500).send(err);
-    }
+
+    const requester = await Friend.findOne({ user: requesterUser });
+    const spender = await Friend.findOne({ user: spenderUser });
+
+    requester.requesting.push(spenderUser);
+    requester.save();
+    spender.spending.push(requesterUser);
+    spender.save();
+
+    res.send({ requester, spender });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // Type:        GET
 // Where:       api/user
 // Purpose:     Deleting friends or cancel friend request from the requester
 // Acess:       Private
-router.post('/delete', async (req, res) => {
-    try {
-        const { requesterUser, spenderUser } = req.body;
+router.post("/delete", async (req, res) => {
+  try {
+    const { requesterUser, spenderUser } = req.body;
 
-        const requester = await Friend.findOne({ user: requesterUser, requesting: spenderUser });
-        const spender   = await Friend.findOne({ user: spenderUser, spending: requesterUser });
+    const requester = await Friend.findOne({
+      user: requesterUser,
+      requesting: spenderUser
+    });
+    const spender = await Friend.findOne({
+      user: spenderUser,
+      spending: requesterUser
+    });
 
-        if (!requester && !spender) {
-            res.send('Users are not friends!');
-        }
-
-        requester.requesting.pull(spenderUser);
-        requester.save()
-        spender.spending.pull(requesterUser);
-        spender.save();
-        res.send({ requester, spender });
+    if (!requester && !spender) {
+      return res.send("Users are not friends!");
     }
-    catch (err) {
-        res.status(500).send(err);
-    }
+
+    requester.requesting.pull(spenderUser);
+    requester.save();
+    spender.spending.pull(requesterUser);
+    spender.save();
+    res.send({ requester, spender });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 module.exports = router;
