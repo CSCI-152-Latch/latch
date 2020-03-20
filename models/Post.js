1ba6538f8b9bad11f93ce7d11cbb5f789364fe51
James Hernandez <hernandezjn97@gmail.com>
2020-03-19 22:34:09 -0700

    completed profile backend
    profile backend routes are completed

--- models/Post.js
+++ models/Post.js
@@ -1,54 +1,62 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const PostSchema = mongoose.Schema({
-    user: {
-        type: String,
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    // Their first name
+    type: String, // Might use Regex
+    required: true
+  },
+  lastName: {
+    // Thier last name
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
         ref: "users"
-    },
-    text: {
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
         type: String,
         required: true
-    },
-    name: {
+      },
+      name: {
         type: String
-    },
-    avatar: {
+      },
+      avatar: {
         type: String
-    },
-    likes: [
-        {
-            user: {
-                type: String,
-                ref: "users"
-            }
-        }
-    ],
-    comments: [
-        {
-            user: {
-                type: String,
-                ref: "users"
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
-    date: {
+      },
+      date: {
         type: Date,
         default: Date.now
+      }
     }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = Post = mongoose.model("post", PostSchema);
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+//app.use("/api/communityForum", require("./routes/api/communityForum"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
