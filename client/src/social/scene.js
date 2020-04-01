6c6d6a176bb488479c96f3a6e3b8659eb79eb212
Khai <zekhai@mail.fresnostate.edu>
2020-03-31 18:04:59 -0700

    create a social scene

---
+++ client/src/social/scene.js
@@ -0,0 +1,9 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+const Social = () => {
+
+}
+
+export default Social;
